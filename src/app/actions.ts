616dd04d23af311a6deab3f0dbb3654570225e19
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-05 21:10:47 +0800

    Implement multi-step application form with validation and file upload functionality

---
+++ src/app/actions.ts
@@ -0,0 +1,166 @@
+"use server"
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { success, z } from 'zod';
+
+const applicationSchema = z.object({
+    name: z.string().min(2, "Full name is required"),
+    age: z.coerce.number().min(1, "Valid age is required"),
+    sex: z.enum(["Male", "Female"]),
+    religion: z.string().optional(),
+    citizenship: z.string().min(1, "Citizenship is required"),
+    bday: z.string().min(1, "Birthday is required"),
+    maritalStatus: z.string().min(1, "Marital status is required"),
+    contact: z.string().min(11, "Contact number must be at least 11 digits"),
+    email: z.string().email("Invalid email format").optional(),
+    address: z.string().min(5, "Full address is required"),
+
+    // --- Educational Info ---
+    schoolType: z.string(),
+    schoolName: z.string(),
+    schoolAddress: z.string(),
+    studentLevel: z.string(),
+    gradeLevel: z.string().optional(),
+    yearLevel: z.string().optional(),
+    course: z.string().optional(),
+    gwa: z.coerce.number().optional(),
+    average: z.coerce.number().optional(),
+
+    // --- Family & Income ---
+    isMotherAlive: z.preprocess((val) => val === 'true' || val === true, z.boolean()).optional(),
+    isFatherAlive: z.preprocess((val) => val === 'true' || val === true, z.boolean()).optional(),
+
+    totalIncome: z.coerce.number().min(0, "Income cannot be negative").optional(),
+    numberOfChild: z.coerce.number().min(0, "Number of children cannot be negative").optional(),
+
+    // Mother Info
+    motherName: z.string().optional(),
+    motherAge: z.coerce.number().optional(),
+    motherAddress: z.string().optional(),
+    motherContact: z.string().optional(),
+    motherOccupation: z.string().optional(),
+    motherEducAttainment: z.string().optional(),
+
+    // Father Info
+    fatherName: z.string().optional(),
+    fatherAge: z.coerce.number().optional(),
+    fatherAddress: z.string().optional(),
+    fatherContact: z.string().optional(),
+    fatherOccupation: z.string().optional(),
+
+    // --- Dependents Array (JSONB) ---
+    dependents: z.array(
+        z.object({
+            childrenName: z.string().min(1, "Dependent name is required"),
+            childrenOccupation: z.string().optional(),
+            childrenYearLevel: z.string().optional()
+        })
+    ).default([]),
+    document_urls: z.array(z.string().url("Must be valid URL")).default([])
+})
+
+export async function submitApplication(formData: FormData) {
+    const rawData = Object.fromEntries(formData.entries());
+
+    try {
+        rawData.dependents = JSON.parse(rawData.dependents as string || '[]');
+    } catch (e) {
+        return { success: false, message: "Failed to read dependents data." };
+    }
+
+    const { coe, cog, validID, ...databaseData } = rawData;
+
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+
+    const validatedFields = applicationSchema.safeParse(databaseData);
+    const cleanData = validatedFields.data;
+
+    const coeFile = coe as File;
+    const cogFile = coe as File;
+    const validIDFile = coe as File;
+    const filesToUpload = [coeFile, cogFile, validIDFile];
+
+    try {
+        const uploadMissions = filesToUpload.map(async (file) => {
+            const fileExt = file.name.split('.').pop();
+            const uniqueFileName = `${Date.now()}-${Math.random().toString(36).substring(7)}.${fileExt}`;
+            const filePath = `${cleanData?.name}/${uniqueFileName}`;
+
+            const { data, error } = await supabase
+                .storage
+                .from('educ-assistance-application-documents')
+                .upload(filePath, file)
+
+            if (error) throw new Error(`Failed to upload ${file.name}`);
+
+            // Get the URL
+            const { data: publicUrlData } = supabase
+                .storage
+                .from('documents')
+                .getPublicUrl(filePath);
+
+            // Return JUST the URL string for this specific file
+            return publicUrlData.publicUrl;
+
+
+        });
+
+        const uploadedUrls = await Promise.all(uploadMissions)
+
+        const validUrls = uploadedUrls.filter((url) => url !== null);
+
+        console.log("All files uploaded in record time!", validUrls);
+
+        const { error: dbError } = await supabase
+            .from("applications")
+            .insert({
+                name: cleanData?.name,
+                age: cleanData?.age,
+                sex: cleanData?.sex,
+                religion: cleanData?.religion,
+                citizenShip: cleanData?.citizenship,
+                bday: cleanData?.bday,
+                marital_status: cleanData?.maritalStatus,
+                contact: cleanData?.contact,
+                email: cleanData?.email,
+                address: cleanData?.address,
+
+                school_type: cleanData?.schoolType,
+                school_name: cleanData?.schoolName,
+                school_address: cleanData?.schoolAddress,
+                student_level: cleanData?.studentLevel,
+                grade_level: cleanData?.gradeLevel,
+                year_level: cleanData?.yearLevel,
+                course: cleanData?.course,
+                gwa: cleanData?.gwa,
+                average: cleanData?.average,
+
+                is_mother_alive: cleanData?.isMotherAlive,
+                is_father_alive: cleanData?.isFatherAlive,
+                total_income: cleanData?.totalIncome,
+                number_of_child: cleanData?.numberOfChild,
+
+                mother_name: cleanData?.motherName,
+                mother_age: cleanData?.motherAge,
+                mother_address: cleanData?.address,
+                mother_contact: cleanData?.motherContact,
+                mother_occupation: cleanData?.motherOccupation,
+                mother_educ_attainment: cleanData?.motherEducAttainment,
+
+                father_name: cleanData?.fatherName,
+                father_age: cleanData?.fatherAge,
+                father_address: cleanData?.fatherAddress,
+                father_contact: cleanData?.fatherContact,
+                father_occupation: cleanData?.fatherOccupation,
+
+                dependents: cleanData?.dependents,
+                document_urls: validUrls
+
+            })
+
+        return { success: true, message: "Application Submission Success!" }
+
+    } catch (error) {
+        console.error("Parallel upload Error:", error)
+        return { success: false, message: "One or more files failed to upload" }
+    }
+}
--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,59 +1,312 @@
 "use client";
 
-import { useState } from "react"
+import { useState, useRef } from "react"
 import style from "./page.module.css"
+import { submitApplication } from "../actions";
 
 export default function ApplicationForm() {
 
+    const [formStep, setFormStep] = useState<number>(1);
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+    const formRef = useRef<HTMLFormElement>(null);
+    const [isCollegeStudent, setIsCollegeStudent] = useState<boolean>(false);
+
+    console.log(isCollegeStudent);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        const formElement = e.currentTarget.closest('form');
         e.preventDefault()
+
+        if (!formElement) return;
+
         setIsSubmitting(true);
-        const formData = new FormData(e.currentTarget);
+        const formData = new FormData(formElement);
 
-        try {
+        const response = await submitApplication(formData);
 
-            setTimeout(() => {
-                console.log("okay na")
-                setIsSubmitting(false);
-                console.log(isSubmitting);
-            }, 2000)
-
-            console.log(formData);
-        } catch (err) {
-            console.log(err);
+        if (response.success) {
+            alert(response.message)
+            setIsSubmitting(false);
+            console.log("Redirect to Thank you page")
+        } else {
+            alert(response.message)
         }
+
+    }
+
+    const handleNext = () => {
+        if (!formRef.current) return;
+
+        const stepInputs = formRef.current.querySelectorAll<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>(
+            `#step-${formStep} input, #step-${formStep} select, #step-${formStep} textarea`
+        );
+
+        for (const input of Array.from(stepInputs)) {
+            if (!input.checkValidity()) {
+                input.reportValidity();
+
+                return;
+            }
+        }
+
+        // 4. If the loop finishes without returning, everything is valid!
+        setFormStep(prev => prev + 1);
+    };
+    const handleBack = () => setFormStep(prevStep => prevStep - 1);
+
+    const handleStudentLevel = (value: string) => {
+        value == "college" ? setIsCollegeStudent(true) : setIsCollegeStudent(false);
     }
 
     return (
         <div>
             <p>Barangay Ayuda Application</p>
-            <form action="POST" onSubmit={handleSubmit}>
-                <label>Name:
-                    <input type="text" className={style.nameInput} />
-                </label>
-                <label>Age:
-                    <input type="number" className={style.ageInput} />
-                </label>
-                <label>Barangay:
-                    <input type="text" className={style.barangayInput} />
-                </label>
-                <label>School/University:
-                    <input type="text" className={style.schoolInput} />
-                </label>
-                <hr />
-                <label>Certificate of Enrollment(file/picture):
-                    <input type="file" className={style.enrollmentFileInput} />
-                </label>
-                <label>Certificate of Grades/Report card(file/picture):
-                    <input type="file" className={style.gradesFileInput} />
-                </label>
-                <label>School ID/Valid ID(file/picture):
-                    <input type="file" className={style.validIDFileInput} />
-                </label>
+            <form action="POST" onSubmit={handleSubmit} ref={formRef}>
+                <div id="step-1" className={style.personalInfoDiv} hidden={formStep != 1}>
+                    <p>PERSONAL INFORMATION</p>
+                    <label>Name:
+                        <input required name="name" type="text" className={style.nameInput} />
+                    </label>
+                    <label>Age:
+                        <input required name="age" type="number" className={style.ageInput} />
+                    </label>
+                    <label>sex:
+                        <input required name="sex" type="text" className={style.sexInput} />
+                    </label>
+                    <label>Religion:
+                        <input name="religion" type="text" className={style.religionInput} />
+                    </label>
+                    <label>Citizenship:
+                        <input required name="citizenship" type="text" className={style.citizenShipInput} />
+                    </label>
+                    <label>Date of Birth:
+                        <input required name="bday" type="date" className={style.bdayInput} />
+                    </label>
+                    <label>Marital Status:
+                        <select required name="maritalStatus" id="maritalStatus" className={style.maritalStatusInput}>
+                            <option value="" disabled selected hidden>Select an option</option>
+                            <option value="single">Single</option>
+                            <option value="married">Married</option>
+                            <option value="separated">Separated</option>
+                            <option value="divorced">Divorced</option>
+                            <option value="widowed">Widowed</option>
+                        </select>
+                    </label>
 
-                <button type="submit" className={style.formSubmitButton}>
+                    <label>Contact number:
+                        <input required name="contact" type="tel" className={style.contactInput} />
+                    </label>
+
+                    <label>Email:
+                        <input name="email" type="email" className={style.emailInput} />
+                    </label>
+
+                    <label>Permanent Address:
+                        <input required name="address" type="text" className={style.permAddressInput} />
+                    </label>
+
+                    <label>School Type:
+                        <select required name="schoolType" id="schoolType" className={style.schoolTypeInput}>
+                            <option value="" disabled selected hidden>Select an option</option>
+                            <option value="private">Private</option>
+                            <option value="public">Public</option>
+                            <option value="vocational">Vocational</option>
+                        </select>
+                    </label>
+
+                    <label>School Name:
+                        <input required name="schoolName" type="text" className={style.schoolNameInput} />
+                    </label>
+
+                    <label>School Address:
+                        <input required name="schoolAddress" type="text" className={style.schoolAddressInput} />
+                    </label>
+
+                    <label>Student Level:
+                        <select onChange={(e) => handleStudentLevel(e.target.value)} required name="studentLevel" id="studentLevel" className={style.studentLevelInput}>
+                            <option value="" disabled selected hidden>Select an option</option>
+                            <option value="junior">Junior High Student</option>
+                            <option value="senior">Senior High Student</option>
+                            <option value="college">College Student</option>
+                        </select>
+                    </label>
+
+                    {isCollegeStudent && (
+                        <>
+                            <label>Year Level:
+                                <select required name="yearLevel" id="yearLevel" className={style.yearLevelInput}>
+                                    <option value="" disabled selected hidden>Select an option</option>
+                                    <option value="y1">1st Year College</option>
+                                    <option value="y2">2nd Year College</option>
+                                    <option value="y3">3rd Year College</option>
+                                    <option value="y4">4th Year College</option>
+                                    <option value="y5">5th Year College</option>
+                                </select>
+                            </label>
+
+                            <label>Course:
+                                <input name="course" type="text" className={style.courseInput} />
+                            </label>
+                            <label>General Weighted Average(GWA):
+                                <input required name="gwa" type="number" step="0.01" className={style.gwaInput} />
+                            </label>
+                        </>
+                    )}
+
+                    {!isCollegeStudent && (
+                        <>
+                            <label>Grade:
+                                <select required name="gradeLevel" id="gradeLevel" className={style.gradeLevelInput}>
+                                    <option value="" disabled selected hidden>Select an option</option>
+                                    <option value="7">Grade 7</option>
+                                    <option value="8">Grade 8</option>
+                                    <option value="9">Grade 9</option>
+                                    <option value="10">Grade 10</option>
+                                    <option value="11">Grade 11</option>
+                                    <option value="12">Grade 12</option>
+                                </select>
+                            </label>
+
+                            <label>Grade Average:
+                                <input required name="average" type="number" step="0.01" className={style.averageInput} />
+                            </label>
+                        </>
+                    )}
+
+                </div>
+
+
+                {/*STEP 2 FAMILY BACKGROUND*/}
+
+                <div id="step-2" className={style.familyBackgorundDiv} hidden={formStep != 2}>
+                    <p>FAMILY BACKGROUND</p>
+                    <label>Mother:
+                        <label htmlFor="aliveTrue">
+                            <input type="radio" id="aliveMotherTrue" name="isMotherAlive" value="true" className={style.aliveInput} />Alive
+                        </label>
+
+                        <label htmlFor="aliveFalse">
+                            <input type="radio" id="aliveMotherFalse" name="isMotherAlive" value="false" className={style.aliveInput} />Deceased
+                        </label>
+                    </label>
+
+                    <label>Father:
+                        <label htmlFor="aliveTrue">
+                            <input type="radio" id="aliveFatherTrue" name="isFatherAlive" value="true" className={style.aliveInput} />Alive
+                        </label>
+
+                        <label htmlFor="aliveFalse">
+                            <input type="radio" id="aliveFatherFalse" name="isFatherAlive" value="false" className={style.aliveInput} />Deceased
+                        </label>
+                    </label>
+
+                    <label>Total Parents Gross Income:
+                        <input name="totalIncome" type="number" className={style.totalIncomeInput} />
+                    </label>
+
+                    <label>Number of Child:
+                        <input required name="numberOfChild" type="number" className={style.numberOfChildInput} />
+                    </label>
+                </div>
+
+                {/*MOTHER INFORMATION*/}
+
+                <div id="step-3" className={style.motherInfoDiv} hidden={formStep != 3}>
+                    <p>MOTHER INFORMATION</p>
+
+                    <label>Name:
+                        <input name="motherName" type="text" className={style.motherNameInput} />
+                    </label>
+                    <label>Age:
+                        <input name="motherAge" type="text" className={style.motherAgeInput} />
+                    </label>
+                    <label>Address:
+                        <input name="motherAddress" type="text" className={style.motherAddressInput} />
+                    </label>
+                    <label>Contact Number:
+                        <input name="motherContact" type="tel" className={style.motherContactInput} />
+                    </label>
+                    <label>Occupation:
+                        <input name="motherOccupation" type="text" className={style.motherOccupationInput} />
+                    </label>
+                    <label>Education Attainment:
+                        <select name="motherEducAttainment" id="motherEducAttainment" className={style.motherEducAttainmentInput}>
+                            <option value="" disabled selected hidden>Select an option</option>
+                            <option value="college">College Graduate</option>
+                            <option value="highSchool">High School Graduate</option>
+                            <option value="elemSchool">Elementary School Graduate</option>
+                        </select>
+                    </label>
+                </div>
+
+                {/*FATHER INFORMATION*/}
+
+                <div id="step-4" className={style.fatherInfoDiv} hidden={formStep != 4}>
+                    <p>FATHER INFORMATION</p>
+
+                    <label>Name:
+                        <input name="fatherName" type="text" className={style.fatherNameInput} />
+                    </label>
+                    <label>Age:
+                        <input name="fatherAge" type="text" className={style.fatherAgeInput} />
+                    </label>
+                    <label>Address:
+                        <input name="fatherAddress" type="text" className={style.fatherAddressInput} />
+                    </label>
+                    <label>Contact Number:
+                        <input name="fatherContact" type="tel" className={style.fatherContactInput} />
+                    </label>
+                    <label>Occupation:
+                        <input name="fatherOccupation" type="text" className={style.fatherOccupationInput} />
+                    </label>
+                    <label>Education Attainment:
+                        <select name="fatherEducAttainment" id="fatherEducAttainment" className={style.fatherEducAttainmentInput}>
+                            <option value="" disabled selected hidden>Select an option</option>
+                            <option value="college">College Graduate</option>
+                            <option value="highSchool">High School Graduate</option>
+                            <option value="elemSchool">Elementary School Graduate</option>
+                        </select>
+                    </label>
+                </div>
+
+                {/*CHILDREN INFORMATION */}
+
+                <div id="step-5" className={style.childrenInfoDiv} hidden={formStep != 5}>
+                    <p>CHILDREN INFORMATION</p>
+                    <label>Name:
+                        <input name="childrenName" type="text" className={style.childrenNameInput} />
+                    </label>
+                    <label>Occupation:
+                        <select name="childrenOccupation" id="childrenOccupation" className={style.childrenOccupationInput}>
+                            <option value="" disabled selected hidden>Select an option</option>
+                            <option value="student">Student</option>
+                            <option value="graduate">Graduate</option>
+                            <option value="employed">Employed</option>
+                        </select>
+                    </label>
+                    <label>Course & Year/ Grade Level:
+                        <input name="childrenYearLevel" type="text" className={style.childrenYearLevelInput} />
+                    </label>
+                </div>
+
+                {/*REQUIREMENT FILES*/}
+
+                <div id="step-6" className={style.requirementFilesDiv} hidden={formStep != 6}>
+                    <p>ADDITIONAL REQUIREMENTS</p>
+                    <label>Certificate of Registration/Enrollment:
+                        <input required name="coe" type="file" className={style.coeInput} />
+                    </label>
+                    <label>Certificate of Grades/Report Card:
+                        <input required name="cog" type="file" className={style.cogInput} />
+                    </label>
+                    <label>School ID/Valid ID:
+                        <input required name="validID" type="file" className={style.idInput} />
+                    </label>
+                </div>
+
+                <button type="button" onClick={handleBack} className={style.backButton} hidden={formStep == 1}>Back</button>
+                <button type="button" onClick={handleNext} className={style.nextButton} hidden={formStep == 6}>Next</button>
+                <button type="submit" className={style.formSubmitButton} hidden={formStep != 6}>
                     {isSubmitting ? "Uploading submission" : "Submit"}
                 </button>
 
