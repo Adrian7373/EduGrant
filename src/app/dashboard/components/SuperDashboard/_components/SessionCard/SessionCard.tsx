267cbf4714353a53f6033fe1dc3be86648c85f01
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-17 22:07:01 +0800

    Add async data fetching in SessionCard; retrieve admin details from Supabase

--- src/app/dashboard/components/SuperDashboard/_components/SessionCard/SessionCard.tsx
+++ src/app/dashboard/components/SuperDashboard/_components/SessionCard/SessionCard.tsx
@@ -1,4 +1,5 @@
 import style from "./SessionCard.module.css";
+import { createClient } from "@/utils/supabase/server";
 
 interface SessionCardProps {
     id: string;
@@ -10,9 +11,20 @@ interface SessionCardProps {
     is_active: boolean;
 }
 
-export default function SessionCard({ id, created_at, name, max_approved,
+export default async function SessionCard({ id, created_at, name, max_approved,
     verification_code, deadline, is_active }: SessionCardProps) {
 
+    const supabase = await createClient();
+
+    const { data: admin } = await supabase
+        .from("batch_admins")
+        .select("admin_id")
+        .eq("batch_id", id)
+        .maybeSingle()
+
+    if (!admin?.admin_id) {
+        console.log("No assigned admin yet.")
+    }
 
 
     return (
