bffbcc5abe5966982318e2219bd711255311eabd
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-18 16:36:37 +0800

    Refactor Configure component to ensure user authentication and role validation

--- src/app/dashboard/configure/page.tsx
+++ src/app/dashboard/configure/page.tsx
@@ -1,6 +1,26 @@
+import { redirect } from "next/navigation";
+import style from "./page.module.css";
+import { createClient } from "@/utils/supabase/server";
 
+export default async function Configure() {
 
-export default function Configure() {
+    const supabase = await createClient();
+    const { data: { user } } = await supabase.auth.getUser();
+    if (!user) {
+        redirect("/login");
+    }
+
+    const { data: profile } = await supabase
+        .from("profiles")
+        .select("role")
+        .eq("id", user.id)
+        .eq("role", "SUPER_ADMIN")
+        .maybeSingle();
+
+    if (!profile) {
+        redirect("/dashboard");
+    }
+
     return (
         <p>Configure session</p>
     )
