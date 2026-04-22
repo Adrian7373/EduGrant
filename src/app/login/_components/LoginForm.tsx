bf2f3105a4a6e590b0a5ef6afaf47b504264dcc0
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-22 21:51:28 +0800

    Add LoginForm component with authentication logic and loader UI

---
+++ src/app/login/_components/LoginForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+import style from "./page.module.css";
+import { authenticateUser } from "@/app/actions";
+import { useTransition, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+
+    const router = useRouter();
+    const [isAuthing, startTransition] = useTransition();
+    const [message, setMessage] = useState<string>();
+
+    const handleSubmit = (formData: FormData) => {
+        startTransition(async () => {
+            const { success, message } = await authenticateUser(formData);
+            setMessage(message);
+            if (success) {
+                router.push("/dashboard");
+            }
+        });
+    };
+
+    return (
+        <div className={style.mainDiv}>
+
+            <div className={style.formDiv} hidden={isAuthing}>
+                <p>Log In</p>
+                <form action={handleSubmit}>
+                    <label>Username:
+                        <input name="email" type="text" />
+                    </label>
+                    <label>Password:
+                        <input name="password" type="text" />
+                    </label>
+                    <p>{message}</p>
+                    <button className={style.loginButton} type="submit" >Login</button>
+                </form>
+            </div>
+
+            {isAuthing && (
+                <>
+                    <span className={style.loader}></span>
+                    <p>Authenticating User</p>
+                </>
+            )}
+
+
+
+        </div>
+    )
+}
