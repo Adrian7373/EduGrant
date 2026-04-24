dee00c7b314cda7b41697baaf0491cbf48ae01c4
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-24 22:28:45 +0800

    Add input field for new admin name in CreateAdminButton modal

--- src/app/dashboard/components/SuperDashboard/_components/CreateAdminButton/CreateAdminButton.tsx
+++ src/app/dashboard/components/SuperDashboard/_components/CreateAdminButton/CreateAdminButton.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 export default function CreateAdminButton() {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [newAdminName, setNewAdminName] = useState("");
 
     return (
         <div className={style.mainDiv}>
@@ -13,7 +14,9 @@ export default function CreateAdminButton() {
             {isOpen && (
                 <div className={style.modalOverlay}>
                     <div className={style.modalContent}>
-
+                        <label>Name:
+                            <input type="text" onChange={(e) => setNewAdminName(e.target.value)} />
+                        </label>
                         <div className={style.modalActions}>
                             <button onClick={() => setIsOpen(false)}>Cancel</button>
                             <button >Create</button>
