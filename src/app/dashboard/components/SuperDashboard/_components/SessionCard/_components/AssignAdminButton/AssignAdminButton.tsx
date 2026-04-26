47cf3d7a675abf6f8274b1ad07f002aec72d9c50
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-26 22:34:56 +0800

    Implement modal functionality in AssignAdminButton component

--- src/app/dashboard/components/SuperDashboard/_components/SessionCard/_components/AssignAdminButton/AssignAdminButton.tsx
+++ src/app/dashboard/components/SuperDashboard/_components/SessionCard/_components/AssignAdminButton/AssignAdminButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import style from "./AssignAdminButton.module.css";
 
 interface Profile {
@@ -10,9 +12,19 @@ interface AssignAdminButtonProps {
 }
 
 export default function AssignAdminButton({ profiles }: AssignAdminButtonProps) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={style.mainDiv}>
-            <button>Assign Admin</button>
+            <button onClick={() => setIsOpen(true)}>Assign Admin</button>
+            {isOpen && (
+                <div className={style.modalOverlay}>
+                    <div className={style.modalContent}>
+
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
