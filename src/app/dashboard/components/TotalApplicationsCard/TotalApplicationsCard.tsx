8753e9c129247f6475abc07afbabb7229754d38a
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-04-28 21:33:07 +0800

    Update text case for "Total applications" in TotalApplicationsCard component

--- src/app/dashboard/components/TotalApplicationsCard/TotalApplicationsCard.tsx
+++ src/app/dashboard/components/TotalApplicationsCard/TotalApplicationsCard.tsx
@@ -3,7 +3,7 @@ import style from "./TotalApplicationsCard.module.css";
 export default function TotalApplicationsCard({ totalCount }: { totalCount: number }) {
     return (
         <div className={style.mainDiv}>
-            <p>Total applications</p>
+            <p>TOTAL APPLICATIONS</p>
             <p>{totalCount}</p>
         </div>
     )
