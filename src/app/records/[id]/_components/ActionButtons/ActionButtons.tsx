a92b1c9457dbc1815509b4334db9439ee70e98a8
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-08 22:40:14 +0800

    Enhance ActionButtons component; add icon to header and improve layout with responsive padding and gap adjustments

--- src/app/records/[id]/_components/ActionButtons/ActionButtons.tsx
+++ src/app/records/[id]/_components/ActionButtons/ActionButtons.tsx
@@ -1,11 +1,12 @@
 "use client";
 import style from "./ActionButtons.module.css";
 import { updateRecordStatus } from "@/app/actions";
+import { SquarePen } from "lucide-react";
 
 export default function ActionButtons({ id }: { id: string }) {
     return (
         <>
-            <p>ACTIONS</p>
+            <p><SquarePen height="3vh" width="1.5vw" />ACTIONS</p>
             <div className={style.buttonsDiv}>
                 <button onClick={() => updateRecordStatus(id, "APPROVED")}>Approve</button>
                 <button onClick={() => updateRecordStatus(id, "REJECTED")}>Reject</button>
