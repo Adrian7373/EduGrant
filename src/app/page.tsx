8feba7cd881b75acd4c1e9cb4eec55d86b0b5750
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-11 10:16:37 +0800

    Improved Login form UI and Landing page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={style.mainDiv}>
-      <p className={style.header}>Gabay Ayuda: Empowering the Future of Our Youth</p>
-      <Link className={style.applyButton} href="/apply">Apply for Assistance</Link>
-      <Link className={style.trackButton} href="/track">Track my Application</Link>
+      <p className={style.header}>EduGrant: Empowering the Future of Our Youth</p>
+      <div className={style.buttonsDiv}>
+        <Link className={style.applyButton} href="/apply">Apply for Assistance</Link>
+        <Link className={style.trackButton} href="/track">Track my Application</Link>
+      </div>
     </div>
   )
 }
