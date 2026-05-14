64a1e5e7129447297f0befd6bff5173c66d80992
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-14 21:32:37 +0800

    Add ConfigureBatchModal component with styles for batch creation functionality

---
+++ src/app/dashboard/components/SuperDashboard/_components/ConfigureBatchModal/ConfigureBatchModal.tsx
@@ -0,0 +1,52 @@
+"use client";
+import style from "./ConfigureBatchModal.module.css";
+import { useState } from "react";
+import BatchForm from "../../../../configure/_components/batchForm";
+
+interface Profile {
+    id: string,
+    name: string
+}
+
+interface ConfigureBatchModalProps {
+    profiles: Profile[] | null
+}
+
+export default function ConfigureBatchModal({ profiles }: ConfigureBatchModalProps) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className={style.mainDiv}>
+            <button onClick={() => setIsOpen(true)}>Create new Batch</button>
+
+            {isOpen && (
+                <div
+                    className={style.modalOverlay}
+                    onClick={() => {
+                        setIsOpen(false);
+                    }}
+                >
+                    <div className={style.modalContent} onClick={(e) => e.stopPropagation()}>
+                        <div className={style.header}>
+                            <p>CREATE A NEW BATCH/SESSION</p>
+                            <button
+                                className={style.closeButton}
+                                type="button"
+                                onClick={() => setIsOpen(false)}
+                            >
+                                ✕
+                            </button>
+                        </div>
+                        <div className={style.formWrapper}>
+                            <BatchForm
+                                initialData={null}
+                                profiles={profiles}
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
