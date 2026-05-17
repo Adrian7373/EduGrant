0c930700da3d53657de678ad2d567ec1ac332632
Adrian7373 <86648061+Adrian7373@users.noreply.github.com>
2026-05-17 20:05:44 +0800

    style: Update page styles and layout for improved aesthetics
    - Added a dark background color to the body.
    - Introduced a fixed logo at the top left corner with responsive sizing.
    - Adjusted header font size for better responsiveness.
    - Simplified button text for clarity and conciseness.
    - Updated button font sizes for improved readability across devices.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import style from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return (
+  return (<>
+    <img className={style.logo} src="EduGrant.svg" alt="edugrant logo" />
     <div className={style.mainDiv}>
-      <p className={style.header}>EduGrant: Empowering the Future of Our Youth</p>
+      <p className={style.header}>Empowering the Future of Our Youth</p>
       <div className={style.buttonsDiv}>
-        <Link className={style.applyButton} href="/apply">Apply for Assistance</Link>
+        <Link className={style.applyButton} href="/apply">Apply</Link>
         <Link className={style.trackButton} href="/track">Track my Application</Link>
       </div>
     </div>
+  </>
   )
 }
